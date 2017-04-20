7bc730b2810377bf96228c748ddb546b6ae0aaa5
luis.felipe <luis.felipe>
2017-04-20 19:01:45 -0300

    alteração

--- treinando_automacao/features/step_definitions/dolar_step.rb
+++ treinando_automacao/features/step_definitions/dolar_step.rb
@@ -8,26 +8,27 @@ Dado(/^que busco no Google pelo site do UOL$/) do
 end
 
 Quando(/^acesso a pagina de economia$/) do
-  find(:css,'#menuHorizontal > div > div > ul > li.menu-uol-item.area-economia > a').click
+  find(:xpath,'//*[@id="menuHorizontal"]/div/div/ul/li[5]/a').click
   sleep 5
 end
 
 Quando(/^busco a cotação do dolar$/) do
     find(:css,'#header-menu > ul > li:nth-child(2) > a').hover
-    sleep 5
-    expect(page).to have_selector(:css,'#header-menu > ul > li.menuItem.left.drop.h-pg-bgcolor1.h-font-color1.com-imagem.transition-025.open > div > ul > li:nth-child(3) > a', visible: true)# check that menu is shown (need to have rspec for this)
-    sleep 5
-    find(:css,'#header-menu > ul > li.menuItem.left.drop.h-pg-bgcolor1.h-font-color1.com-imagem.transition-025.open > div > ul > li:nth-child(3) > a').click
     sleep 3
-    @dolar_atual = find(:css, '#conteudo-principal > div.wrap > div.cinza > div.infoTable > table > tbody > tr > td.ask').text
+    expect(page).to have_selector(:xpath,'//*[@id="header-menu"]/ul/li[2]/div/ul/li[3]/a', visible: true)# check that menu is shown (need to have rspec for this)
+    sleep 5
+    find(:xpath,'//*[@id="header-menu"]/ul/li[2]/div/ul/li[3]/a').click
+    sleep 3
+    @dolar_atual = find(:xpath, '//*[@id="conteudo-principal"]/div[1]/div[2]/div[1]/table/tbody/tr/td[3]').text
     puts @dolar_atual
 
 end
 
 Então(/^comparo se o valor é menor que "([^"]*)"$/) do |dolar|
     @dolar_previsto = dolar
-    if @dolar_atual < @dolar_previsto
-      puts true
+    puts @dolar_previsto
+    if @dolar_atual.to_f < @dolar_previsto.to_f
+      puts true 
     else
       puts false
     end
